76b3d02362ec5ba311904702794066ff8100b5cf
j22188530 <berkayascioglu@std.iyte.edu.tr>
2020-10-19 09:31:37 +0000

    .

--- lab2/example2.py
+++ lab2/example2.py
@@ -2,5 +2,5 @@ x = float(input("Enter the x: "))
 y = float(input("Enter the y: "))
 z = float(input("Enter the z: "))
 
-denklem = ((2*x + y)**2)*z*0.5/((x*0.5)+(y*0.5))
+denklem = ((2*x + y)**2)*z**0.5/((x**0.5)+(y**0.5))
 print(denklem)
